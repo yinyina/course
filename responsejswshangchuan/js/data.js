419b84cb7e474cf963a4c4d2102f3bb7218be0df
yinyina <49050734+yinyina@users.noreply.github.com>
2019-12-04 11:29:34 +0800

    Add files via upload

---
+++ responsejswshangchuan/js/data.js
@@ -0,0 +1,48 @@
+//课程代码
+var CourseCode = "xkbg0101"
+
+//课程名称
+var CourseName = "解读高中英语学科核心素养"
+
+//一级栏目名称：ppt栏目有两个及以上时必须使用含有ppt的英文，且ppt栏目下只能放ppt
+var arrMenuName = ["视频学习-sp","课程简介-kj","课程导学-dx","专家简介-zj","授课PPT-ppt","拓展资料-tz"] 
+
+//一级栏目类型：视频是video,页面是html,下载是down
+var arrMenuStyle = ["video","html","html","html","html","html","html","html","html","html"] 
+
+//二级栏目名称：
+var arrddName1 = ["第一节","第二节","第三节","第四节","第五节"]
+var arrddName2
+var arrddName3
+var arrddName4
+var arrddName5 
+var arrddName6 = ["小结","下载资料"]
+var arrddName7 
+var arrddName8 
+var arrddName9 
+var arrddName10 
+
+
+//二级栏目类型：二级栏目类型相同并且与一级栏目类型相同时，则可不写，二级栏目类型不同时则每个都写
+var arrddStyle1
+var arrddStyle2 
+var arrddStyle3 
+var arrddStyle4
+var arrddStyle5 
+var arrddStyle6 = ["html","down"]
+var arrddStyle7  
+var arrddStyle8
+var arrddStyle9 
+var arrddStyle10
+
+//cc视频vid： 可以没有vid，但是数组[]符号须保留
+var arrVid1 = ["E7892FC60D15C4CC9C33DC5901307461","514EC5DA7766511B9C33DC5901307461","57E17C9DAC67CEA79C33DC5901307461","71DF37EF6474BC8B9C33DC5901307461","2AD0C479267FD2329C33DC5901307461"]
+var arrVid2 = []
+var arrVid3 = []
+var arrVid4 = []
+var arrVid5 = []
+var arrVid6 = []
+var arrVid7 = []
+var arrVid8 = []
+var arrVid9 = []
+var arrVid10 = []
